b96460f384ac7ca1f697e123a684cd195ec80fb3
chengs <sczeros@163.com>
2019-05-28 17:05:30 +0800

    lcd

--- main/main.js
+++ main/main.js
@@ -1,4 +1,27 @@
-module.exports = function main() {
+module.exports = function main(a) {
     console.log("Debug Info");
-    return 'Hello World!';
+    var line1 = ["._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."];
+    var line2 = ["|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|"];
+    var line3 = ["|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|"];    
+    
+    var len = a.length;
+    var array = new Array(len);
+    for (i = 0; i < len; i++) {
+        array[i] = a.charAt(i);
+    }
+    var res1 = "";
+    var res2 = "";
+    var res3 = "";
+    for (i = 0; i < len; i++) {
+            res1 += line1[array[i]] + " ";
+    }
+    for (i = 0; i < len; i++) {
+            res2 += line2[array[i]] + " ";
+    }
+    for (i = 0; i < len; i++) {
+            res3 += line3[array[i]] + " ";
+    }
+    var res = res1.trim() + "\n" + res2.trim()+ "\n" + res3.trim() + "\n";
+    console.log(res);
+    return res;
 };
